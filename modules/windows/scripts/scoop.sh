512091b62405bed38339d1fea90ac490c57e2170
Brian Choy <bycEEE@gmail.com>
2023-07-18 14:27:13 -0700

    Add windows applications

---
+++ modules/windows/scripts/scoop.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+if ! scoop > /dev/null 2>&1; then
+    echo "Scoop command not found, make sure Scoop is installed and in your PATH"
+    exit 1
+fi
+
+echo "Updating Scoop..."
+scoop update
+
+echo "Scoop updated successfully."
+
+# Declare the bucket details in an associative array
+declare -A buckets
+
+buckets["extras"]="https://github.com/ScoopInstaller/Extras"
+buckets["games"]="https://github.com/Calinou/scoop-games"
+buckets["main"]="https://github.com/ScoopInstaller/Main.git"
+buckets["versions"]="https://github.com/ScoopInstaller/Versions"
+
+# Loop over the associative array and add each bucket
+for bucket in "${!buckets[@]}"; do
+    echo "Adding bucket: $bucket"
+    scoop bucket add $bucket ${buckets[$bucket]}
+done
+
+echo "All buckets added successfully."
+
+
+apps=(
+    "7zip"
+    "aria2"
+    "curl"
+    "fiddler"
+    "gpg"
+    "gpu-z"
+    "hxd"
+    "mpc-be"
+    "musicbee"
+    "qbittorrent"
+    "raindrop.io"
+    "rufus"
+    "ryujinx"
+    "sharex"
+    "spacesniffer"
+    "ssd-z"
+    "sudo"
+    "sysinternals"
+    "yuzu"
+    "zeal"
+)
+
+for app in "${apps[@]}"; do
+    echo "Installing app: $app"
+    scoop install $app
+    echo "Updating app: $app"
+    scoop update $app
+done
+
+echo "All apps added and updated successfully."
---
+++ modules/windows/scripts/winget.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+if ! winget.exe > /dev/null 2>&1; then
+    echo "winget.exe command not found, make sure winget is installed and in your PATH"
+    exit 1
+fi
+
+apps=(
+    # Security
+    "AgileBits.1Password"
+    "Malwarebytes.Malwarebytes"
+
+    # Browsers
+    "Google.Chrome"
+    "Mozilla.Firefox"
+
+    # General Applications
+    "Spotify.Spotify"
+    "Elgato.StreamDeck"
+    "Kairos.DuetDisplay"
+    "Logitech.GHUB"
+    "Obsidian.Obsidian"
+
+    # Development
+    "chrisant996.Clink"
+    "Docker.DockerDesktop"
+    "Microsoft.WindowsTerminal"
+    # "Nvidia.CUDA"
+
+    # System Monitoring
+    "CPUID.HWMonitor"
+    "CrystalDewWorld.CrystalDiskMark"
+    "Piriform.Speccy"
+
+    # Gaming
+    "FlawlessWidescreen.FlawlessWidescreen"
+    "NexusMods.Vortex"
+    "Nvidia.GeForceExperience"
+    "Valve.Steam"
+
+    # Messaging
+    "Discord.Discord"
+    "Facebook.Messenger"
+    "SlackTechnologies.Slack"
+
+    # Utilities
+    "Guru3D.Afterburner"
+    "LukaszSwiszcz.Winpinator"
+    "Microsoft.PowerToys"
+    "schollz.croc"
+
+    # Misc
+    "Gyan.FFmpeg"
+    "jstarks.npiperelay"
+    "Microsoft.DotNet.DesktopRuntime.3_1"
+    # "Microsoft.DotNet.DesktopRuntime.5"
+    "Microsoft.DotNet.DesktopRuntime.6"
+    "Microsoft.DotNet.DesktopRuntime.7"
+    "Microsoft.dotNetFramework"
+)
+
+for app in "${apps[@]}"; do
+    echo "Installing app: $app"
+    winget.exe install --silent -e --id "$app" --no-upgrade --accept-source-agreements --accept-package-agreements
+done
+
+echo "All apps added and updated successfully."
+
+winget.exe upgrade
