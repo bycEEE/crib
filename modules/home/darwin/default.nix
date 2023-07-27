f6a47230bc05f3db30ef0b01a88996cb596b99cc
Brian Choy <bycEEE@gmail.com>
2023-07-26 23:42:24 -0700

    Back up iterm2, karabiner, linearmouse

--- modules/home/darwin/default.nix
+++ modules/home/darwin/default.nix
@@ -1,6 +1,12 @@
-{...}: {
+{config, ...}: let
+  configPath = "${config.home.homeDirectory}/crib/modules/home/darwin/config";
+in {
   imports = [
     ./preferences.nix
     # ./homebrew.nix
   ];
+
+  xdg.configFile."iterm2/com.googlecode.iterm2.plist".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/iterm2/com.googlecode.iterm2.plist";
+  xdg.configFile."karabiner".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/karabiner";
+  xdg.configFile."linearmouse".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/linearmouse";
 }
