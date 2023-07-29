83d90374d5b2f9bcb1900d76a20b19f82951eddf
Brian Choy <bycEEE@gmail.com>
2023-07-28 17:07:51 -0700

    Remove configurations

--- modules/home/darwin/default.nix
+++ modules/home/darwin/default.nix
@@ -6,9 +6,4 @@ in {
     ./preferences.nix
     # ./homebrew.nix
   ];
-
-  xdg.configFile."iterm2/com.googlecode.iterm2.plist".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/iterm2/com.googlecode.iterm2.plist";
-  xdg.configFile."karabiner".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/karabiner";
-  xdg.configFile."linearmouse".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/linearmouse";
-  home.file."${libraryPath}/Preferences/com.raycast.macos.plist".source = config.lib.file.mkOutOfStoreSymlink "${configPath}/raycast/com.raycast.macos.plist";
 }
