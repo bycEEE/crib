6c850fd2baf012ad13703edc2849e3b110c8b36d
Brian Choy <bycEEE@gmail.com>
2023-10-12 15:03:49 -0700

    Update zsh plugins

--- modules/home/cli/shell.nix
+++ modules/home/cli/shell.nix
@@ -127,8 +127,8 @@ in {
         src = pkgs.fetchFromGitHub {
           owner = "chisui";
           repo = "zsh-nix-shell";
-          rev = "v0.5.0";
-          sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+          rev = "v0.7.0";
+          sha256 = "sha256-oQpYKBt0gmOSBgay2HgbXiDoZo5FoUKwyHSlUrOAP5E=";
         };
       }
       # {
