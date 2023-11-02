27ac7fc708c318dafd1bfd5d4d03f3d0a51c1c9c
Brian Choy <bycEEE@gmail.com>
2023-11-02 16:36:31 -0700

    Fix braa

--- modules/home/cli/braa.nix
+++ modules/home/cli/braa.nix
@@ -1,29 +1,32 @@
 {
   lib,
   stdenv,
-  fetchurl,
+  fetchzip,
   zlib,
 }:
 stdenv.mkDerivation rec {
   pname = "braa";
   version = "0.82";
 
-  src = fetchurl {
+  src = fetchzip {
     url = "http://s-tech.elsat.net.pl/${pname}/${pname}-${version}.tar.gz";
-    sha256 = "d650d34cdf30fcbbbbf4e4f11c8f5d51fd5caa3cca58d620fa1d978b8c18919c";
+    hash = "sha256-GS3kk432BdGx/sLzzjXvotD9Qn4S3U4XtMmM0fWMhGA=";
   };
 
   buildInputs = [zlib];
 
   installPhase = ''
+    runHook preInstall
     install -Dm755 braa $out/bin/braa
+    runHook postInstall
   '';
 
   meta = with lib; {
-    description = "A mass snmp scanner.";
+    description = "A mass snmp scanner";
     homepage = "http://s-tech.elsat.net.pl";
-    license = licenses.gpl2;
+    license = licenses.gpl2Only;
     platforms = platforms.unix;
     maintainers = with maintainers; [bycEEE];
+    mainProgram = "braa";
   };
 }
