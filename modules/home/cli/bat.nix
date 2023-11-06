c45474632c0db394972598e73ae8a2c58621fa8c
Brian Choy <bycEEE@gmail.com>
2023-11-06 14:49:54 -0800

    Add theme to bat

--- modules/home/cli/bat.nix
+++ modules/home/cli/bat.nix
@@ -1,11 +1,17 @@
 {pkgs, ...}: {
+  xdg.configFile."bat/themes/Catppuccin-macchiato.tmTheme".text = builtins.readFile (pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/catppuccin/bat/477622171ec0529505b0ca3cada68fc9433648c6/Catppuccin-macchiato.tmTheme";
+    hash = "sha256-76fS4lq8obgOAYaKSVqBc2wOP+RLUCeTQL69vrUfs3k=";
+  });
+  xdg.configFile."bat/themes/Catppuccin-macchiato.tmTheme".onChange = "${pkgs.bat}/bin/bat cache --build";
+
   programs.bat = {
     enable = true;
     config = {
       pager = "never";
       style = "plain";
       # color = "always";
-      theme = "base16";
+      theme = "Catppuccin-macchiato";
     };
     extraPackages = with pkgs.bat-extras; [
       batdiff
