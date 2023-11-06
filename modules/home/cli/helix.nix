5aa7bf8da88b077a99c798cab6ee493ebfb3426a
Brian Choy <bycEEE@gmail.com>
2023-11-06 15:19:37 -0800

    Add commented out language settings

--- modules/home/cli/helix.nix
+++ modules/home/cli/helix.nix
@@ -44,6 +44,62 @@
         };
       };
 
+      # languages = {
+      #   language = let
+      #     prettier = lang: {
+      #       command = "${pkgs.nodePackages.prettier}/bin/prettier";
+      #       args = ["--parser" lang];
+      #     };
+      #     prettierLangs = map (e: {
+      #       name = e;
+      #       formatter = prettier e;
+      #     });
+      #     langs = ["css" "scss" "json" "html"];
+      #   in
+      #     [
+      #       {
+      #         name = "bash";
+      #         auto-format = true;
+      #         formatter = {
+      #           command = "${pkgs.shfmt}/bin/shfmt";
+      #           args = ["-i" "2"];
+      #         };
+      #       }
+      #       {
+      #         name = "clojure";
+      #         injection-regex = "(clojure|clj|edn|boot|yuck)";
+      #         file-types = ["clj" "cljs" "cljc" "clje" "cljr" "cljx" "edn" "boot" "yuck"];
+      #       }
+      #     ]
+      #     ++ prettierLangs langs;
+
+      #   language-server = {
+      #     bash-language-server = {
+      #       command = "${pkgs.nodePackages.bash-language-server}/bin/bash-language-server";
+      #       args = ["start"];
+      #     };
+
+      #     clangd = {
+      #       command = "${pkgs.clang-tools}/bin/clangd";
+      #       clangd.fallbackFlags = ["-std=c++2b"];
+      #     };
+
+      #     nil = {
+      #       command = lib.getExe pkgs.nil;
+      #       config.nil.formatting.command = ["${lib.getExe pkgs.alejandra}" "-q"];
+      #     };
+
+      #     vscode-css-language-server = {
+      #       command = "${pkgs.nodePackages.vscode-css-languageserver-bin}/bin/css-languageserver";
+      #       args = ["--stdio"];
+      #       config = {
+      #         provideFormatter = true;
+      #         css.validate.enable = true;
+      #       };
+      #     };
+      #   };
+      # };
+
       # vim-like key bindings from https://github.com/LGUG2Z/helix-vim/blob/master/config.toml
       keys.normal = {
         # Quick iteration on config changes
