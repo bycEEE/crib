45aa2895d606f4a477e81e9dadd4789c1b9cb60c
Brian Choy <bycEEE@gmail.com>
2023-11-06 15:11:01 -0800

    Fix windows downloading, add better clear type

--- modules/windows/scripts/github.sh
+++ modules/windows/scripts/github.sh
@@ -29,9 +29,17 @@ fi
 app="dlss-swapper"
 if ! check_existing_folder $app; then
     echo "Downloading $app"
-    curl -s -L https://github.com/ililim/dual-key-remap/releases/download/v0.7/dual-key-remap-v0.7.zip -o /tmp/dual-key-remap.zip
-    ouch decompress /tmp/dual-key-remap.zip --dir $bin_path/${app}
-    rm -rf /tmp/dual-key-remap.zip
+    curl -s -L https://github.com/beeradmoore/dlss-swapper/releases/download/v1.0.2.0/DLSS.Swapper-1.0.2.0-portable.zip -o /tmp/dlss-swapper.zip
+    ouch decompress /tmp/dlss-swapper.zip --dir $bin_path/${app}
+    rm -rf /tmp/dlss-swapper.zip
+fi
+
+app="BetterClearTypeTuner"
+if ! check_existing_folder $app; then
+    echo "Downloading $app"
+    curl -s -L https://github.com/bp2008/BetterClearTypeTuner/releases/download/1.4.0.2/BetterClearTypeTuner.1.4.0.2.zip -o /tmp/BetterClearTypeTuner.zip
+    ouch decompress /tmp/BetterClearTypeTuner.zip --dir $bin_path/${app}
+    rm -rf /tmp/BetterClearTypeTuner.zip
 fi
 
 # app="dlss-swapper"
