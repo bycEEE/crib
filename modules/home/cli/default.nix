da9d9f45adeb861bd9c75d12332aaa65b554d1f6
Brian Choy <bycEEE@gmail.com>
2023-11-08 21:17:48 -0800

    Go back to moar

--- modules/home/cli/default.nix
+++ modules/home/cli/default.nix
@@ -223,7 +223,7 @@ Useful links:
     sessionVariables = {
       EDITOR = "helix";
       VISUAL = "helix";
-      PAGER = "less";
+      PAGER = "moar";
       # NODE_PATH = "${NODE_GLOBAL}/lib";
       # JAVA_HOME = "${pkgs.jdk11}";
       # PNPM_HOME = "${PNPM_DIR}";
