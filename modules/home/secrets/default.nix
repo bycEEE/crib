d43c5c105fe61d17002a5f8635a0deb0169871f1
Brian Choy <bycEEE@gmail.com>
2023-11-10 16:52:31 -0800

    Update secrets

--- modules/home/secrets/default.nix
+++ modules/home/secrets/default.nix
@@ -9,13 +9,17 @@
   ];
 
   age.identityPaths = [
-    "${config.home.homeDirectory}/.ssh/id_ed25519"
+    "${config.home.homeDirectory}/.ssh/id_ed25519-agenix" # main-agenix
   ];
 
   age.secrets = {
     "aws.config" = {
       path = "${config.home.homeDirectory}/.aws/config";
       file = "${mystash}/aws.config.age";
+    };
+    "aws.credentials" = {
+      path = "${config.home.homeDirectory}/.aws/credentials";
+      file = "${mystash}/aws.credentials.age";
     };
   };
 }
