4faf42167aadc8b56e580fcbfad3ad3c8ed4c0f5
Brian Choy <bycEEE@gmail.com>
2023-11-10 19:18:42 -0800

    Comment out secrets

--- modules/home/secrets/default.nix
+++ modules/home/secrets/default.nix
@@ -5,22 +5,22 @@
   mystash,
   ...
 }: {
-  imports = [
-    agenix.homeManagerModules.default
-  ];
+  # imports = [
+  #   agenix.homeManagerModules.default
+  # ];
 
-  age.identityPaths = [
-    "${config.home.homeDirectory}/.ssh/id_ed25519-agenix" # main-agenix
-  ];
+  # age.identityPaths = [
+  #   "${config.home.homeDirectory}/.ssh/id_ed25519-agenix" # main-agenix
+  # ];
 
-  age.secrets = {
-    "aws.config" = {
-      path = "${config.home.homeDirectory}/.aws/config";
-      file = "${mystash}/aws.config.age";
-    };
-    "aws.credentials" = {
-      path = "${config.home.homeDirectory}/.aws/credentials";
-      file = "${mystash}/aws.credentials.age";
-    };
-  };
+  # age.secrets = {
+  #   "aws.config" = {
+  #     path = "${config.home.homeDirectory}/.aws/config";
+  #     file = "${mystash}/aws.config.age";
+  #   };
+  #   "aws.credentials" = {
+  #     path = "${config.home.homeDirectory}/.aws/credentials";
+  #     file = "${mystash}/aws.credentials.age";
+  #   };
+  # };
 }
