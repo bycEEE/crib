e723278972493dfb83d7e7c8bb78516d1df78a32
Brian Choy <bycEEE@gmail.com>
2024-05-28 23:02:06 -0700

    Add OSINT

--- hosts/BREEZY.nix
+++ hosts/BREEZY.nix
@@ -4,6 +4,10 @@
       # todo add https://github.com/stealthsploit/OneRuleToRuleThemStill
       # john # John the Ripper password cracker
       # hashcat # Fast password cracker
+
+      # OSINT
+      maigret # Collect a dossier on a person by username from thousands of sites
+      sherlock # Find usernames across social networks
     ];
   };
 }
