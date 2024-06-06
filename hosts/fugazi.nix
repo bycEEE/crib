6f91693655a29e5a0a245a04d8c18dd6b5bd853c
Brian Choy <bycEEE@gmail.com>
2024-06-05 18:29:04 -0700

    Update tools

--- hosts/fugazi.nix
+++ hosts/fugazi.nix
@@ -18,10 +18,6 @@
       stegseek # Fast steghide cracker that can be used to extract hidden data from files
       firefox_decrypt # Decrypt user data from Firefox profile directory
 
-      # Web
-      subfinder # Subdomain discovery tool
-      wtfis # Passive hostname, domain and IP lookup tool for non-robots
-
       # Vulnerability Scanner
       nuclei # Fast and customizable vulnerability scanner based on simple YAML based DSL
       wapiti # Web application vulnerability scanner
@@ -31,8 +27,13 @@
       feroxbuster # Fast, simple, recursive content discovery tool
       ffuf # Fast web fuzzer written in Go
       gobuster # Tool used to brute-force URIs, DNS subdomains, Virtual Host names on target web servers
+      subfinder # Subdomain discovery tool
+      wtfis # Passive hostname, domain and IP lookup tool for non-robots
       wfuzz # Web content fuzzer to facilitate web applications assessments
       whatweb # Next generation web scanner
+
+      # SMB
+      enum4linux-ng # Windows/Samba enumeration tool
 
       # Utility
       nmap-formatter # A tool that allows you to convert NMAP results to html, csv, json, markdown, graphviz (dot) or sqlite. Simply put it's nmap converter.
