7aa0b67f7154948c75051e2b604c75e44a4edea8
Brian Choy <bycEEE@gmail.com>
2024-12-25 16:21:10 -0500

    Update nerdfonts

--- modules/home/fonts/default.nix
+++ modules/home/fonts/default.nix
@@ -10,18 +10,14 @@ Useful links:
   home = {
     packages = with pkgs; [
       ### Fonts
-      (nerdfonts.override {
-        fonts = [
-          "Hack"
-          "FiraCode"
-          "JetBrainsMono"
-          "Inconsolata"
-          "SourceCodePro"
-          "Iosevka"
-          "Agave"
-          "Monaspace"
-        ];
-      })
+      nerd-fonts.hack
+      nerd-fonts.fira-code
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.inconsolata
+      nerd-fonts.sauce-code-pro
+      nerd-fonts.iosevka
+      nerd-fonts.agave
+      nerd-fonts.monaspace
       (pkgs.callPackage ./sf-mono-nerdfont.nix {})
       # (pkgs.callPackage ./apple-fonts.nix {})
     ];
