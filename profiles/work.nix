e83c3eebbfcf3c402592a848f214a12247d89604
Brian Choy <bycEEE@gmail.com>
2026-04-27 18:21:14 -0400

    Update signing key

--- profiles/work.nix
+++ profiles/work.nix
@@ -2,10 +2,10 @@
   programs.git = {
     settings = {
       user.name = "Brian Choy";
-      user.email = "bycEEE@gmail.com";
+      user.email = "brian@alaffia.io";
     };
     signing = {
-      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINWcwuVWLxEG4/dbXI/yA+v8g5CGXeFg0VBVvYlLfHcO";
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDzMHZwClyJHxko4fYErJAF9iLOB+zRMWFKparZ4s9F4";
       signByDefault = true;
     };
   };
