e47132373b33db52e9f75b7adb016c08246b4947
Brian Choy <bycEEE@gmail.com>
2026-04-28 17:46:31 -0400

    Fix SSH_AUTH_SOCK for macOS

--- modules/home/cli/ssh.nix
+++ modules/home/cli/ssh.nix
@@ -1,14 +1,8 @@
 {
-  isVm,
   lib,
   pkgs,
   ...
-}: let
-  ssh_auth_sock =
-    if pkgs.stdenvNoCC.isDarwin
-    then "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-    else "~/.ssh/agent.sock";
-in {
+}: {
   programs.ssh = {
     enable = true;
     # forwardAgent = true;
@@ -23,4 +17,6 @@ in {
       PreferredAuthentications publickey,password
     '';
   };
+
+  home.sessionVariables.SSH_AUTH_SOCK = lib.mkIf pkgs.stdenvNoCC.isDarwin "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
 }
