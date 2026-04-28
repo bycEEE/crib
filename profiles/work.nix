7f65ae792ee5b44b3a7a8b98dd0c88c35a4a1bff
Brian Choy <bycEEE@gmail.com>
2026-04-28 17:55:05 -0400

    Change git user email

--- profiles/work.nix
+++ profiles/work.nix
@@ -2,7 +2,7 @@
   programs.git = {
     settings = {
       user.name = "Brian Choy";
-      user.email = "brian@alaffia.io";
+      user.email = "bycEEE@gmail.com";
     };
     signing = {
       key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDzMHZwClyJHxko4fYErJAF9iLOB+zRMWFKparZ4s9F4";
