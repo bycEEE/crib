2c3f536226eea8ecdc7c555fc95c9cd2c7036c9a
Brian Choy <bycEEE@gmail.com>
2026-05-04 16:12:17 -0400

    Create allowed_signers file

--- profiles/personal.nix
+++ profiles/personal.nix
@@ -1,12 +1,22 @@
-{...}: {
+{config, ...}: let
+  name = "Brian Choy";
+  email = "bycEEE@gmail.com";
+  pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAqeyKQGFLXGhNpu6RwFfbJdofRlfj0aLO0iVvWAyp52";
+in {
   programs.git = {
     settings = {
-      user.name = "Brian Choy";
-      user.email = "bycEEE@gmail.com";
+      user.name = name;
+      user.email = email;
     };
     signing = {
-      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAqeyKQGFLXGhNpu6RwFfbJdofRlfj0aLO0iVvWAyp52";
+      key = pubkey;
       signByDefault = true;
     };
+    settings = {
+      gpg.ssh.allowedSignersFile = "${config.home.homeDirectory}/.ssh/allowed_signers";
+    };
   };
+  home.file.".ssh/allowed_signers".text = ''
+    ${email} namespaces="git" ${pubkey}
+  '';
 }
--- profiles/work.nix
+++ profiles/work.nix
@@ -1,12 +1,22 @@
-{...}: {
+{config, ...}: let
+  name = "Brian Choy";
+  email = "bycEEE@gmail.com";
+  pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDzMHZwClyJHxko4fYErJAF9iLOB+zRMWFKparZ4s9F4";
+in {
   programs.git = {
     settings = {
-      user.name = "Brian Choy";
-      user.email = "bycEEE@gmail.com";
+      user.name = name;
+      user.email = email;
     };
     signing = {
-      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDzMHZwClyJHxko4fYErJAF9iLOB+zRMWFKparZ4s9F4";
+      key = pubkey;
       signByDefault = true;
     };
+    settings = {
+      gpg.ssh.allowedSignersFile = "${config.home.homeDirectory}/.ssh/allowed_signers";
+    };
   };
+  home.file.".ssh/allowed_signers".text = ''
+    ${email} namespaces="git" ${pubkey}
+  '';
 }
