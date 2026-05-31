ccbb9f0eda54b14d512c06aa20ca283908c517bf
Brian Choy <bycEEE@gmail.com>
2026-05-30 21:43:15 -0400

    Update ghostty

--- modules/home/cli/ghostty.nix
+++ modules/home/cli/ghostty.nix
@@ -12,9 +12,13 @@
       font-size = 13.5;
       font-thicken = true;
       font-thicken-strength = 128;
+      macos-titlebar-style = "tabs";
+      window-padding-x = 10;
+      window-padding-y = 10;
+      unfocused-split-opacity = 0.5;
 
       working-directory = "inherit";
-      scrollback-limit = 10000000; # In bytes, not lines
+      scrollback-limit = 50000000; # In bytes, not lines
       bell-features = "system,audio,attention,title";
       copy-on-select = "clipboard";
       window-colorspace = "display-p3";
