8a1bcda41202bca5a49c6a1624eeb5ff0326a88e
Brian Choy <bycEEE@gmail.com>
2026-06-01 13:35:24 -0400

    Move cspell config location

--- modules/home/cli/cspell.nix
+++ modules/home/cli/cspell.nix
@@ -6,5 +6,5 @@
   home.packages = with pkgs; [
     cspell # Spell checker for code
   ];
-  xdg.configFile."cspell/.cspell.config.yaml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/crib/modules/home/cli/.cspell.config.yaml";
+  xdg.configFile."cspell/.cspell.config.yaml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/crib/modules/home/cli/scripts/.cspell.config.yaml";
 }
