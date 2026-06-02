1037e71576130f4e5cd45be8e099a15f396c0a42
Brian Choy <bycEEE@gmail.com>
2026-06-01 21:26:16 -0400

    Remove just formatter

--- modules/home/cli/lsp.nix
+++ modules/home/cli/lsp.nix
@@ -2,7 +2,6 @@
   home.packages = with pkgs; [
     # Formatters
     alejandra # A formatter for nixpkgs
-    just-formatter # Justfile formatter
     nixpkgs-fmt # nix code formatter that would be applied on nixpkgs
     # nodePackages.prettier # An opinionated code formatter
     shfmt # A shell parser, formatter, and interpreter (POSIX/Bash/mksh)
