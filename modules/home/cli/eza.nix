abfb6cd772e1276b9b04e2f5db82d240e300d14d
Brian Choy <bycEEE@gmail.com>
2026-06-01 21:06:08 -0400

    Update eza

--- modules/home/cli/eza.nix
+++ modules/home/cli/eza.nix
@@ -2,6 +2,12 @@
   programs.eza = {
     enable = true;
     icons = "auto";
+    extraOptions = [
+      "--color=always"
+      "--group-directories-first"
+      "--header"
+      "--time-style=long-iso"
+    ];
   };
   catppuccin.eza.enable = true;
 }
